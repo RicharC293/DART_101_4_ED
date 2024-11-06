b0ce9a0e7a67d06d5fdb84de41d28383e4f7400a
wmazon19811 <wmazon1980@gmail.com>
2024-11-05 21:17:17 -0500

    clase 4

---
+++ 02_VARIABLES/WALTER_MAZON/CLASE/variables.dart
@@ -0,0 +1,20 @@
+
+
+String global="Esta es una variable global";
+void main()
+{
+  //var
+  var nombre="Walter";
+  
+  //late
+  late String edad;
+  edad="12";
+  print(edad);
+
+  // es recomendable utilizar las variables siempre con el tipo de dato
+  String apellido="Mazon";
+
+
+  const valocidadSonido=250;
+  final nombre="hola";
+}
