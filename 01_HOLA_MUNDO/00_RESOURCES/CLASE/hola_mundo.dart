0ad955d475c55f93ec4511ded5596423564f2faa
wmazon19811 <wmazon1980@gmail.com>
2024-11-06 21:01:43 -0500

    Merge remote-tracking branch 'upstream/main'

--- 01_HOLA_MUNDO/00_RESOURCES/CLASE/hola_mundo.dart
+++ 01_HOLA_MUNDO/00_RESOURCES/CLASE/hola_mundo.dart
@@ -1,13 +1,4 @@
-// void -> no siempre es necesario
-
-<<<<<<< HEAD
 void main() 
 {
-=======
-void main() {
->>>>>>> upstream/main
-  // Cuerpo de la función
-  print("Hola Mundo");
-
-  print("     Hola Richar :D         \n Hola otra vez");
+print ("hola mundo");
 }
--- 02_VARIABLES/WALTER_MAZON/CLASE/variables.dart
+++ 02_VARIABLES/WALTER_MAZON/CLASE/variables.dart
@@ -1,20 +1,7 @@
 
 
-String global="Esta es una variable global";
 void main()
 {
-  //var
-  var nombre="Walter";
-  
-  //late
-  late String edad;
-  edad="12";
-  print(edad);
-
-  // es recomendable utilizar las variables siempre con el tipo de dato
-  String apellido="Mazon";
-
-
-  const valocidadSonido=250;
-  final nombre="hola";
+  string nombre="hola";
+  print(nombre);
 }
