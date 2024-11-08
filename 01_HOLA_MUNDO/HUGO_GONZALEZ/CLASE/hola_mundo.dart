638b853cc2ea9f21a7ca83b7a5a3a8b612285263
HeyHugoGonzalez <hugogonzalez@hey.com>
2024-11-07 19:08:03 -0500

    hola

---
+++ 01_HOLA_MUNDO/HUGO_GONZALEZ/CLASE/hola_mundo.dart
@@ -0,0 +1,11 @@
+/// Hola Mundo en Dart
+/// void -> no te retorna nada
+/// main () -> Funcion prinicpal
+/// { -> bloque de la funcion o cuerpo
+///
+///
+/// }
+void main() {
+  /// print ->funion u objeo que va a imrimir un valor en consola
+  print("Hola Mundo, me llamo HUGO  ");
+}
