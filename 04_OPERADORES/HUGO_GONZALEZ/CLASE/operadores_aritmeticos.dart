d9f6b244f91ec0433a8d03bafb3e0cde52fee4ec
HeyHugoGonzalez <hugogonzalez@hey.com>
2024-11-12 18:56:01 -0500

    Probando

--- 04_OPERADORES/HUGO_GONZALEZ/CLASE/operadores_aritmeticos.dart
+++ 04_OPERADORES/HUGO_GONZALEZ/CLASE/operadores_aritmeticos.dart
@@ -44,4 +44,63 @@ void main() {
   // split separa todos los elementos con un string
   print(mensaje2.split(""));
 
+
+
+  // restar
+  print("---------");
+  print ("REsta");
+ 
+  /// falta ....
+  /// 
+  /// 
+  /// 
+  
+  /*
+
+  // Multiplicacion
+  print()"multiplicacion");
+
+  /// falta ....
+  /// 
+  /// 
+  /// 
+  */
+
+
+  /// // Division
+  /// print("--------");
+  /// print("Division");
+
+  final variable7 = 7;
+  final variable8 =2;
+  // El valor de retorno tipo double
+  final division = variable7 / variable8;
+  print (division);
+
+  // tenemos la posibilidad de que el resultado solo sea la parte entera
+  // ~/ -> ASCII: alt + 126
+ 
+  final divisionEntera = variable7 ~/ variable8;
+  // 7/2 -> 3.5
+  // 7~/2 -> 3  -> Parte entera
+
+  print(divisionEntera);
+
+  // MOdulo %
+  // 7 | 2 
+  // -6    3 -> parte entera
+  // 1 -> modulo
+  // validacion -> saber si la division es exacta
+    // numeros primos
+    // Par - Impar
+    final divisionModulo 
+    
+    /// ----------------------
+    /// FAlta ejemplos de incrementos y decrementos
+    /// 
+    /// +
+
+
 }
+
+  
